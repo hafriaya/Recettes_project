600fa9d3443f285fd569832d3c30962338aa65f6
Aya Hafri <115592428+hafriaya@users.noreply.github.com>
2025-05-10 21:28:52 +0100

    adjust_urls_&_views

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
-from .ressources.recettes import recettes
-from .models import Recette 
-# Create your views here.
+from .models import Recette
+
 class Home(View):
-    def get(self,request):
-        return render(request,'hello.html',{})
+    def get(self, request):
+        return render(request, 'hello.html', {})
+
 class ListeRecettes(View):
     def get(self, request):
-        recettes = Recette.objects.all()  # Fetch all Recette objects from the database
+        recettes = Recette.objects.all()
         return render(request, 'listeRecettes.html', {'recettes': recettes})
+
+class WhatsInMyFridge(View):
+    def get(self, request):
+        return render(request, 'fridge.html', {})
+
+    def post(self, request):
+        ingredients = request.POST.getlist('ingredients')
+        recettes = Recette.objects.filter(
+            ingredients__icontains=','.join(ingredients)
+        )
+        return render(request, 'fridge_results.html', {'recettes': recettes})
+
+class SearchRecettes(View):
+    def get(self, request):
+        query = request.GET.get('q', '')
+        recettes = Recette.objects.filter(
+            nom__icontains=query
+        ) | Recette.objects.filter(
+            ingredients__icontains=query
+        )
+        return render(request, 'search_results.html', {'recettes': recettes, 'query': query})
